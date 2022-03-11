dfa574795da1a13a1745a34cb77b16984b6c0e9e
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-11 15:19:50 -0500

    Add currency context to access current currency

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import './App.css';
 // components
 import Container from './components/Container';
 import Display from './components/Display';
+// contexts
+import { currencyContext } from './contexts/CurrencyContext';
 
 function App() {
   const [budget, setBudget] = useState(0);
   const [totalIncome, setTotalIncome] = useState(0);
   const [totalExpenses, setTotalExpenses] = useState(0);
+  const [currency, setCurrency] = useState('$');
 
   useEffect(() => {
     setBudget(totalIncome - totalExpenses);
   }, [totalIncome, totalExpenses]);
 
-  return (
+  return <currencyContext.Provider value={{ currency, setCurrency }}>
     <div className="App">
       <Display budget={budget} />
 
       <Container type="income" setTotal={setTotalIncome} />
       <Container type="expenses" setTotal={setTotalExpenses} />
     </div>
-  );
+  </currencyContext.Provider>;
 }
 
 export default App;
