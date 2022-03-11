711e3d1172feeca436b27c32a788f1497f190304
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-11 14:16:12 -0500

    Display total budget above expenses

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 // components
 import Container from './components/Container';
+// icons
+import { BiLineChart, BiLineChartDown } from 'react-icons/bi';
 
 function App() {
   const [budget, setBudget] = useState(0);
@@ -14,6 +16,27 @@ function App() {
 
   return (
     <div className="App">
+      <div className="display">
+        <h2>
+          {/* this is so it does not display Budget: $-123 for negative budgets */}
+          Budget: {budget >= 0 ? `$${budget}` : `-$${budget.toString().substring(1)}`}
+          <br />
+          {budget >= 0 ?
+          // display if budget is positive
+          <>
+            <BiLineChart className="green" />
+            <BiLineChart className="green" />
+            <BiLineChart className="green" />
+          </> :
+          // display is budget is negative
+          <>
+            <BiLineChartDown className="red" />
+            <BiLineChartDown className="red" />
+            <BiLineChartDown className="red" />
+          </>}
+        </h2>
+      </div>
+
       <Container type="income" setTotal={setTotalIncome} />
       <Container type="expenses" setTotal={setTotalExpenses} />
     </div>
