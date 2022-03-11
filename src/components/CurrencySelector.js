8de22cf66ca9d6a033c6316aa35c5fd9e8db607a
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-11 18:49:20 -0500

    Display currency buttons and make container scrollable

--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import getSymbolFromCurrency from 'currency-symbol-map'
+import currencyData from '../currencyData';
 
 const CurrencySelector = ({ setShowModal }) => {
+  const [lookup, setLookup] = useState('');
+
   return (
     <div className="currency-selector" onMouseLeave={() => setShowModal(false)}>
-      {/* stuff goes here */}
+      <input
+        className="lookup"
+        value={lookup}
+        onChange={e => setLookup(e.target.value)} />
+      <div className="currencies-grid">
+        {Object.keys(currencyData).map(currency => {
+          return (
+            getSymbolFromCurrency(currencyData[currency])!==undefined &&
+            <button>{getSymbolFromCurrency(currencyData[currency])}</button>
+          );
+        })}
+      </div>
     </div>
   );
 };
