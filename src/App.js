b4c29c2523d025e03bc6895992857302431c8f70
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-11 10:33:17 -0500

    Add budget state to keep track of budget

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // components
 import Container from './components/Container';
 
 function App() {
+  const [budget, setBudget] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
+
+  useEffect(() => {
+    setBudget(totalIncome - totalExpenses);
+  }, [totalIncome, totalExpenses]);
+
   return (
     <div className="App">
-      <Container type="income" />
-      <Container type="expenses" />
+      <Container type="income" setTotal={setTotalIncome} />
+      <Container type="expenses" setTotal={setTotalExpenses} />
     </div>
   );
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // componenets
 import TitleBar from './titlebar/TitleBar';
 import BudgetPiece from './BudgetPiece';
 // icons
 import { FaPlus } from 'react-icons/fa';
 
-const EarningsContainer = ({ type }) => {
+const EarningsContainer = ({ type, setTotal }) => {
   const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    let sum = 0;
+    expenses.forEach(expense => {
+      // sum the expenses
+      sum += +expense.value.substring(1);
+    });
+    setTotal(sum);
+  }, [expenses]);
 
   const addExpense = () => {
     setExpenses([...expenses, {desc: "", value: "", id: new Date().getTime()}]);
