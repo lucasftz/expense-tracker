5cd959cbc7c4b36ca1b8c490357b8d7576affe2a
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-11 20:32:39 -0500

    Fix bug where you were unable to add an expense with a multi-char currency

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -18,8 +18,9 @@ const Expense = ({ type, expense, expenses, setExpenses, removeExpense }) => {
 
   const handleValueChange = (e) => {
     // find last character
+    const lastCurrencyChar = currency.charAt(currency.length - 1);
     let lastChar = e.target.value.charAt(e.target.value.length-1);
-    if (lastChar===currency && e.target.value.length===1) lastChar=0
+    if (lastChar===lastCurrencyChar && e.target.value.length===currency.length) lastChar=0
     // if last char is not numeric, do nothing (don't prepend it to desc)
     // prev line is to change the initial currency symbol to a numeric char if it's the only char
     if (isNaN(+lastChar)) return;
@@ -28,9 +29,9 @@ const Expense = ({ type, expense, expenses, setExpenses, removeExpense }) => {
     // add what was typed to the copy
     expenseCopy.value = e.target.value;
     // if there is no initial currency symbol, prepend it
-    if (expenseCopy.value[0] !== currency) expenseCopy.value = currency + expenseCopy.value;
+    if (expenseCopy.value.substring(0, currency.length) !== currency) expenseCopy.value = currency + expenseCopy.value;
     // if there is only one char (which will be '$', delete it)
-    if (expenseCopy.value.length === 1) expenseCopy.value = "";
+    if (expenseCopy.value.length === currency.length) expenseCopy.value = "";
     setExpenses(expenses.map(exp => exp.id === expense.id ? expenseCopy : exp));
   };
 
